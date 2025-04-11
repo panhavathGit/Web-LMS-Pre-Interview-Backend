bced908e6651e9bf81cef32aa5b4e5d6efaa357c
panhavath <chanpanhavath@gmail.com>
2025-04-11 10:11:21 +0700

    Finish authentication

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
+Route::get('/test-api', function () {
+    return response()->json(['message' => 'API is working']);
+});
